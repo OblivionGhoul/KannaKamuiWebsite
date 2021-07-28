f605b8ffd180b818b57f4fb62822eb73c8d3a077
Minh Dinh <mdinh49@gmail.com>
2021-07-28 11:10:09 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -4,7 +4,6 @@ module.exports = (app, express) => {
     app.use(express.static('public'))
 
     let port = process.env.PORT || 3000
-
     app.listen(port, function () {
         console.log('App is running, server is on port', port)
     })
